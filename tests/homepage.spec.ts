d6f0368d2229dce1f533fd8d37258090a77e403c
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2023-01-05 17:53:03 +0200

    add playwright tests

---
+++ tests/homepage.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("homepage has title", async ({ page }) => {
+  await page.goto("/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Group Survivors/);
+});
+
+test("homepage has canvas", async ({ page }) => {
+  await page.goto("/");
+
+  const canvas = page.getByTestId("canvas");
+  await expect(canvas).toHaveAttribute("id", "canvas");
+});
