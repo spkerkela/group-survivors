9810b621e08f011c8837b56b54b8936875bf13a2
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2025-12-19 12:58:26 +0200

    switch default port since 3000 is used by everything

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:4000",
     /* Navigation timeout */
     navigationTimeout: 30000,
 
@@ -114,7 +114,7 @@ const config: PlaywrightTestConfig = {
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run server",
-    url: "http://localhost:3000",
+    url: "http://localhost:4000",
     reuseExistingServer: !process.env.CI,
     timeout: 1000 * 60,
   },
--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,5 @@
 export const serverTimeScale = 1;
 export const playersRequired =
   Number.parseInt(process.env.PLAYERS_REQUIRED ?? "1", 10) || 1;
-export const port = Number.parseInt(process.env.PORT ?? "3000", 10) || 3000;
+export const port = Number.parseInt(process.env.PORT ?? "4000", 10) || 4000;
 export const host = process.env.HOST || "localhost";
